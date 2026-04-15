6f620ef6eaa21c9de84d81fa11b6218f957210f7
Pasha <balanenko12@gmail.com>
2026-04-15 17:32:54 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,27 @@ export const isActive = ({ isActive }: { isActive: boolean }) =>
   });
 
 export const App = () => (
-  <>
-    <html className="has-navbar-fixed-top">
-      <nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink to="/" className={isActive}>
-              Home
-            </NavLink>
-            <NavLink to="/tabs" className={isActive}>
-              Tabs
-            </NavLink>
-          </div>
-        </div>
-      </nav>
-
-      <div className="section">
-        <div className="container">
-          <Outlet />
+  <div className="has-navbar-fixed-top">
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink to="/" className={isActive} end>
+            Home
+          </NavLink>
+          <NavLink to="/tabs" className={isActive}>
+            Tabs
+          </NavLink>
         </div>
       </div>
-    </html>
-  </>
+    </nav>
+
+    <div className="section">
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
+  </div>
 );
--- src/TabsPage.tsx
+++ src/TabsPage.tsx
@@ -1,5 +1,8 @@
 import { Link, useParams } from 'react-router-dom';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
@@ -9,35 +12,46 @@ export const tabs = [
 export const TabsPage: React.FC = () => {
   const { tabId } = useParams<{ tabId: string }>();
 
-  const selectedTab = tabs.find(tab => tab.id === tabId);
+  const selectedIndex = tabs.findIndex(tab => tab.id === tabId);
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
 
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              className={tab.id === tabId ? 'is-active' : ''}
-              data-cy="Tab"
-            >
-              <Link to={`/tabs/${tab.id}`} className="navbar-item">
-                {tab.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* Используем компоненты библиотеки вместо ручных <ul> */}
+      <Tabs
+        selectedIndex={selectedIndex === -1 ? 0 : selectedIndex}
+        onSelect={() => {}}
+      >
+        <div className="tabs is-boxed">
+          <TabList>
+            {tabs.map(tab => (
+              <Tab key={tab.id}>
+                <Link
+                  to={`/tabs/${tab.id}`}
+                  className="navbar-item"
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                  {tab.title}
+                </Link>
+              </Tab>
+            ))}
+          </TabList>
+        </div>
 
-      <div className="block" data-cy="TabContent">
-        {selectedTab ? (
-          <p>{selectedTab.content}</p>
-        ) : (
-          <p>Please select a tab</p>
-        )}
-      </div>
+        {/* Контент табов */}
+        <div className="block" data-cy="TabContent">
+          {selectedIndex !== -1 ? (
+            tabs.map(tab => (
+              <TabPanel key={tab.id}>
+                <p>{tab.content}</p>
+              </TabPanel>
+            ))
+          ) : (
+            <p>Please select a tab</p>
+          )}
+        </div>
+      </Tabs>
     </>
   );
 };
